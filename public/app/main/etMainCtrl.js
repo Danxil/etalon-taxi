9992bc9584dfec6a76fbdca8edb9fbd3c72f0b09
Joseph <umnick.jo@mail.ru>
2015-06-05 19:25:34 +0300

    order form

--- public/app/main/etMainCtrl.js
+++ public/app/main/etMainCtrl.js
@@ -1,2 +1,15 @@
 angular.module('app').controller('etMainCtrl', function ($scope, $http, $rootScope) {
+    $scope.map = {
+        center: {latitude: 47.181485, longitude: 8.519028}
+    };
+
+    $scope.price = {
+        firstDigit: 0,
+        secondDigit: 1,
+        thirdDigit: 2
+    };
+
+    $scope.toShowNumber = function () {
+        return ($scope.whereFromStreet && $scope.whereFromHouse) && ($scope.withoutFinalPoint || ($scope.whereToStreet && whereToHouse))
+    }
 });
