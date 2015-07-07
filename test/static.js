8ff721454150be436983a45d2cf479e85ef7b284
danxil <danxil@list.ru>
2015-07-07 14:38:19 +0300

    fix

---
+++ test/static.js
@@ -0,0 +1,22 @@
+var request = require('request');
+
+var options = {
+    proxy: process.env.QUOTAGUARDSTATIC_URL,
+    url: 'https://api.github.com/repos/joyent/node',
+    headers: {
+        'User-Agent': 'node.js'
+    }
+};
+
+describe('test static:', function () {
+    it('should return success', function (done) {
+        request(options, function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(options.proxy);
+                done();
+            } else {
+                done(error);
+            }
+        });
+    });
+});
